a9191d65896f0cb5433bede53cb52d24ea7350d2
Jacudibu <jacudibu@gmail.com>
2018-08-19 18:28:04 +0200

    Added Mineral Harvesting

--- src/prototypes/room.mineral.js
+++ src/prototypes/room.mineral.js
@@ -4,7 +4,6 @@ Object.defineProperty(Room.prototype, 'mineral', {
     get: function() {
         if (!this._mineral) {
             if (roomMineralIds[this.name] === undefined) {
-                console.log("allocating global mineral memory");
                 let [mineral] = this.find(FIND_MINERALS);
                 if (!mineral) {
                     roomMineralIds[this.name] = null;
