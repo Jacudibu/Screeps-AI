5f8d0546960247512d6d86000d30d6eaad6c934f
Jacudibu <jacudibu@gmail.com>
2018-08-31 16:38:22 +0200

    Performance boost for lab code, also fixed some minor errors.

--- src/labreactionrunner.js
+++ src/labreactionrunner.js
@@ -4,6 +4,14 @@ const IDLE_TIME_IF_NO_MATCH = 1000;
 const IDLE_TIME_ON_GLOBAL_RESET = 20;
 const IDLE_TIME_IF_NO_DEMAND = 250;
 
+global.REACTIONS_INVERTED = {};
+for (let mineralA in REACTIONS) {
+    for (let mineralB in REACTIONS[mineralA]) {
+        let product = REACTIONS[mineralA][mineralB];
+        REACTIONS_INVERTED[product] = [mineralA, mineralB];
+    }
+}
+
 const labReactionRunner = {
     run: function() {
         for (let roomName in Game.rooms) {
@@ -49,7 +57,7 @@ const labReactionRunner = {
                 this.makeEmpty(room);
                 break;
             case LABTASK.BOOST_CREEP:
-            // TODO: Ship needed minerals to labs.
+            // TODO: Ship needed minerals to labs if something is in boostQueue
 
             default:
                 room.memory.labtask = LABTASK.DECIDE_WHAT_TO_DO;
@@ -58,15 +66,21 @@ const labReactionRunner = {
     },
 
     doLabReactions: function(room) {
+        let result;
         for (let lab of room.outputLabs) {
-            lab.runReaction(room.inputLabs[0], room.inputLabs[1]);
+            result = lab.runReaction(room.inputLabs[0], room.inputLabs[1]);
         }
 
-        if (room.inputLabs[0].mineralAmount === 0 || room.inputLabs[1].mineralAmount === 0) {
-            room.memory.labtask = LABTASK.DECIDE_WHAT_TO_DO;
+        switch (result) {
+            case ERR_NOT_ENOUGH_RESOURCES:
+                room.memory.labtask = LABTASK.DECIDE_WHAT_TO_DO;
+                break;
+            case ERR_INVALID_ARGS:
+                room.memory.labtask = LABTASK.MAKE_EMPTY;
+                break;
         }
 
-        nextLabTick[room.name] = Game.time + room.outputLabs[0].cooldown;
+        nextLabTick[room.name] = Game.time + REACTION_TIME[REACTIONS[room.inputLabs[0].requestedMineral][room.inputLabs[1].requestedMineral[1]]];
     },
 
     decideWhatToDo: function(room) {
@@ -80,12 +94,13 @@ const labReactionRunner = {
             }
         }
 
-        // Is product still in demand?
+        // Check if demand is empty due to global reset
         if (Object.keys(resourceDemand).length === 0) {
             nextLabTick[room.name] = Game.time + IDLE_TIME_ON_GLOBAL_RESET;
             return;
         }
 
+        // Is product still in demand?
         if (resourceDemand[room.name] === undefined) {
             nextLabTick[room.name] = Game.time + IDLE_TIME_IF_NO_DEMAND;
             room.memory.labtask = LABTASK.MAKE_EMPTY;
@@ -105,16 +120,13 @@ const labReactionRunner = {
             }
         }
 
-        console.log("keep current reaction -> " + keepCurrentReaction);
         if (keepCurrentReaction) {
             room.memory.labtask = LABTASK.RUN_REACTION;
             nextLabTick[room.name] = Game.time + IDLE_TIME_IF_WAITING_FOR_RESOURCES;
         } else {
             if (this.areLabsEmpty(room)) {
-                console.log("labs marked as empty");
                 this.determineReactionMaterials(room);
             } else {
-                console.log("labs marked as full, make them empty");
                 room.memory.labtask = LABTASK.MAKE_EMPTY;
                 room.inputLabs[0].requestedMineral = null;
                 room.inputLabs[1].requestedMineral = null;
@@ -125,36 +137,26 @@ const labReactionRunner = {
     determineReactionMaterials: function(room) {
         const demandWithoutBaseMinerals = resourceDemand[room.name].filter(demand => !BASE_MINERALS.includes(demand.resourceType));
 
-        // TODO: this can probable be made a whole lot nicer by adding our own global format for reactions which uses products as KEYS
-        const reactionKeys = Object.keys(REACTIONS);
         for (let demand of demandWithoutBaseMinerals) {
-            for (let i = 0; i < reactionKeys.length; i++) {
-                const subKeys = Object.keys(REACTIONS[reactionKeys[i]]);
-                for (let j = 0; j < subKeys.length; j++) {
-                    let currentReaction = REACTIONS[reactionKeys[i]][subKeys[j]];
-                    if (demand.resourceType !== currentReaction) {
-                        continue;
-                    }
-
-                    if (!room.storage.store[reactionKeys[i]] && !room.terminal.store[reactionKeys[i]]) {
-                        continue;
-                    }
-
-                    if (!room.storage.store[subKeys[j]] && !room.terminal.store[subKeys[j]]) {
-                        continue;
-                    }
-
-                    // ITS POSSIBLE!
-                    room.inputLabs[0].requestedMineral = reactionKeys[i];
-                    room.inputLabs[1].requestedMineral = subKeys[j];
-                    room.memory.labtask = LABTASK.RUN_REACTION;
-                    nextLabTick[room.name] = Game.time + IDLE_TIME_IF_WAITING_FOR_RESOURCES;
-                    return;
-                }
+            let mineralA = REACTIONS_INVERTED[demand.resourceType][0];
+            if (!room.storage.store[mineralA] && !room.terminal.store[mineralA]) {
+                continue;
             }
+
+            let mineralB = REACTIONS_INVERTED[demand.resourceType][1];
+            if (!room.storage.store[mineralB] && !room.terminal.store[mineralB]) {
+                continue;
+            }
+
+            // Found something we can produce!
+            room.inputLabs[0].requestedMineral = mineralA;
+            room.inputLabs[1].requestedMineral = mineralB;
+            room.memory.labtask = LABTASK.RUN_REACTION;
+            nextLabTick[room.name] = Game.time + IDLE_TIME_IF_WAITING_FOR_RESOURCES;
+            return;
         }
 
-        // No possible productions found, check back later... WAY LATER.
+        // No possible productions found, check back later - WAY LATER.
         nextLabTick[room.name] = Game.time + IDLE_TIME_IF_NO_MATCH;
     },
 
