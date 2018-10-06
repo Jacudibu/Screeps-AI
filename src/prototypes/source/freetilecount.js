5e47eb44638acc20dcde5acda90899e66f873559
Jacudibu <jacudibu@gmail.com>
2018-10-06 22:18:56 +0200

    removed the last use of Game.map.getTerrainAt

--- src/prototypes/source/freetilecount.js
+++ src/prototypes/source/freetilecount.js
@@ -10,10 +10,11 @@ Object.defineProperty(Source.prototype, "freeTileCount", {
 });
 
 Source.prototype._countFreeTilesAroundSource = function() {
+    const terrain = this.room.getTerrain();
     let freeTileCount = 0;
     [this.pos.x - 1, this.pos.x, this.pos.x + 1].forEach(x => {
         [this.pos.y - 1, this.pos.y, this.pos.y + 1].forEach(y => {
-            if (Game.map.getTerrainAt(x, y, this.pos.roomName) !== 'wall')
+            if (terrain.get(x, y) !== TERRAIN_MASK_WALL)
                 freeTileCount++;
         }, this);
     }, this);
