2853b53541fbfe9c07be467b49492aeb3d3553fe
Jacudibu <jacudibu@gmail.com>
2018-11-02 15:37:00 +0100

    Added a Threat Response Stat to rooms.

--- src/prototypes/room/threat.js
+++ src/prototypes/room/threat.js
@@ -2,19 +2,25 @@ Room.prototype.checkForHostiles = function() {
     this._dangerousHostiles = this.find(FIND_HOSTILE_CREEPS, {
         filter: creep =>
             creep.pos.x !== 0 && creep.pos.x !== 49 && creep.pos.y !== 0 && creep.pos.y !== 49
-            &&(creep.countBodyPartsOfType(ATTACK) > 0
-            || creep.countBodyPartsOfType(RANGED_ATTACK) > 0
-            || creep.countBodyPartsOfType(HEAL) > 0
-            || creep.countBodyPartsOfType(WORK) > 4 && creep.countBodyPartsOfType(CARRY) < 4)
+            && (creep.isMeleeAttacker()
+            ||  creep.isRangedAttacker()
+            ||  creep.isHealer()
+            ||  creep.isDismantler()
+            ||  creep.canAttackController())
     });
 };
 
 global.roomThreats = {};
+global.roomThreatResponses = {};
 
 Room.prototype.updateThreat = function() {
+    this.updateHostileThreat();
+    this.updateThreatResponse();
+};
+
+Room.prototype.updateHostileThreat = function() {
     if (this._dangerousHostiles.length === 0) {
         roomThreats[this.name] = null;
-        return;
     }
 
     let threat = {
@@ -43,7 +49,7 @@ Room.prototype.updateThreat = function() {
         threat.heal   += creep.countActiveBodyPartsOfTypeAndApplyBoostWeighting(HEAL);
         threat.tough  += creep.countActiveBodyPartsOfTypeAndApplyBoostWeighting(TOUGH);
         threat.claim  += creep.countActiveBodyPartsOfTypeAndApplyBoostWeighting(CLAIM);
-        threat.work  += creep.countActiveBodyPartsOfTypeAndApplyBoostWeighting(WORK);
+        threat.work   += creep.countActiveBodyPartsOfTypeAndApplyBoostWeighting(WORK);
         threat.other  += creep.countBodyPartsOfType(MOVE) + creep.countBodyPartsOfType(CARRY); // cheaper calculation as no benefit for fighting
         threat.total  += creep.body.length;
     }
@@ -54,6 +60,49 @@ Room.prototype.updateThreat = function() {
     roomThreats[this.name] = threat;
 };
 
+
+Room.prototype.updateThreatResponse = function() {
+    if (this._dangerousHostiles.length === 0) {
+        roomThreatResponses[this.name] = null;
+        return;
+    }
+
+    const availableDefenders = this.find(FIND_MY_CREEPS, {
+        filter: c => c.isMeleeAttacker() || c.isRangedAttacker() || c.isHealer()
+    });
+
+    if (availableDefenders.length === 0) {
+        roomThreatResponses[this.name] = null;
+        return;
+    }
+
+    let threatResponse = {
+        creepCount: 0,
+        attack: 0,
+        ranged: 0,
+        heal: 0,
+        tough: 0,
+        work: 0,
+        claim: 0,
+        other: 0,
+        total: 0,
+    };
+
+    for (let creep of availableDefenders) {
+        threatResponse.creepCount++;
+        threatResponse.attack += creep.countActiveBodyPartsOfTypeAndApplyBoostWeighting(ATTACK);
+        threatResponse.ranged += creep.countActiveBodyPartsOfTypeAndApplyBoostWeighting(RANGED_ATTACK);
+        threatResponse.heal   += creep.countActiveBodyPartsOfTypeAndApplyBoostWeighting(HEAL);
+        threatResponse.tough  += creep.countActiveBodyPartsOfTypeAndApplyBoostWeighting(TOUGH);
+        threatResponse.work   += creep.countActiveBodyPartsOfTypeAndApplyBoostWeighting(WORK);
+        threatResponse.claim  += creep.countActiveBodyPartsOfTypeAndApplyBoostWeighting(CLAIM);
+        threatResponse.other  += creep.countBodyPartsOfType(MOVE) + creep.countBodyPartsOfType(CARRY); // cheaper calculation as no benefit for fighting
+        threatResponse.total  += creep.body.length;
+    }
+
+    roomThreatResponses[this.name] = threatResponse;
+};
+
 Room.prototype.askForHelpIfThreatDetected = function() {
     if (!roomThreats[this.name]) {
         this.memory.requiresHelp = undefined;
@@ -61,10 +110,9 @@ Room.prototype.askForHelpIfThreatDetected = function() {
     }
 
     if (this.memory.requiresHelp === undefined) {
-        const myDefenseForce = this.find(FIND_MY_CREEPS, {filter: creep => creep.role === ROLE.DEFENDER || creep.role === ROLE.RANGED_DEFENDER});
-        if (myDefenseForce.length === 0) {
+        if (!roomThreatResponses[this.name]) {
             this.memory.requiresHelp = true;
-            if (   roomThreats.onlyNPCs
+            if (  !roomThreats.onlyNPCs
                 && this.controller
                 &&(this.controller.my || this.controller.reservation && this.controller.reservation.username === "Jacudibu")) {
 
