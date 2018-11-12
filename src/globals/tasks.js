b98d0d22af5eaadf0e35e58613826051be873177
Jacudibu <jacudibu@gmail.com>
2018-11-12 21:21:25 +0100

    Tasks are now sorted alphabetically

--- src/globals/tasks.js
+++ src/globals/tasks.js
@@ -1,41 +1,42 @@
 global.TASK = {};
-global.TASK.HARVEST_ENERGY = 'harvest_energy';
-global.TASK.HAUL_ENERGY = 'haul_energy';
-global.TASK.MOVE_ONTO_CONTAINER = 'move_onto_container';
-global.TASK.STORE_ENERGY = 'store_energy';
-global.TASK.COLLECT_ENERGY = 'collect_energy';
-global.TASK.UPGRADE_CONTROLLER = 'upgrade_controller';
-global.TASK.SIGN_CONTROLLER = 'sign_controller';
-global.TASK.RENEW_CREEP = 'renew_creep';
-global.TASK.BUILD_STRUCTURE = 'build_structure';
-global.TASK.REPAIR_STRUCTURE = 'repair_structure';
-global.TASK.DECIDE_WHAT_TO_DO = 'decide_what_to_do';
-global.TASK.MOVE_TO_ROOM = 'move_to_room';
-global.TASK.WAIT_FOR_INPUT = 'wait_for_input';
+
 global.TASK.ATTACK = 'attack';
-global.TASK.DISMANTLE = 'dismantle';
+global.TASK.ATTACK_CONTROLLER = 'attack_controller';
+global.TASK.BUILD_STRUCTURE = 'build_structure';
 global.TASK.CLAIM_CONTROLLER = 'claim_controller';
-global.TASK.RESERVE_CONTROLLER = 'reserve_controller';
-global.TASK.RECYCLE = 'recycle';
-global.TASK.DEFEND_RAMPARTS = 'defend_ramparts';
+global.TASK.COLLECT_ENERGY = 'collect_energy';
+global.TASK.DECIDE_WHAT_TO_DO = 'decide_what_to_do';
 global.TASK.DEFEND_CHARGE = 'defend_charge';
-global.TASK.HARVEST_MINERAL = 'harvest_mineral';
+global.TASK.DEFEND_RAMPARTS = 'defend_ramparts';
+global.TASK.DISMANTLE = 'dismantle';
+global.TASK.HAUL_ENERGY = 'haul_energy';
 global.TASK.HAUL_RESOURCE = 'haul_resource';
+global.TASK.HARVEST_ENERGY = 'harvest_energy';
+global.TASK.HARVEST_MINERAL = 'harvest_mineral';
+global.TASK.HEAL = "heal";
+global.TASK.MOVE_ONTO_CONTAINER = 'move_onto_container';
+global.TASK.MOVE_TO_ROOM = 'move_to_room';
+global.TASK.RECYCLE = 'recycle';
+global.TASK.RENEW_CREEP = 'renew_creep';
+global.TASK.REPAIR_STRUCTURE = 'repair_structure';
+global.TASK.RESERVE_CONTROLLER = 'reserve_controller';
+global.TASK.SIGN_CONTROLLER = 'sign_controller';
+global.TASK.STANDBY = "standbyTalk";
+global.TASK.STOMP_HOSTILE_CONSTRUCTION_SITES = "stomp_hostile_construction_sites";
+global.TASK.STORE_ENERGY = 'store_energy';
 global.TASK.STORE_MINERAL = 'store_mineral';
 global.TASK.STORE_RESOURCE = 'store_resource';
-global.TASK.ATTACK_CONTROLLER = 'attack_controller';
+global.TASK.UPGRADE_CONTROLLER = 'upgrade_controller';
 global.TASK.WAIT = 'wait';
-global.TASK.STOMP_HOSTILE_CONSTRUCTION_SITES = "stomp_hostile_construction_sites";
-global.TASK.HEAL = "heal";
-global.TASK.STANDBY = "standbyTalk";
+global.TASK.WAIT_FOR_INPUT = 'wait_for_input';
+
 
 global.TASK.CHECK_REMOTE_ROAD_REGENERATION = "check_remote_road_regeneration";
 global.TASK.DISABLE_ATTACK_NOTIFICATION = "disable_notification";
 
 
-
 global.LABTASK = {};
-global.LABTASK.RUN_REACTION = "run_reaction";
 global.LABTASK.BOOST_CREEP = "boost_creep";
 global.LABTASK.DECIDE_WHAT_TO_DO = "decide_what_to_do";
 global.LABTASK.MAKE_EMPTY = "make_empty";
+global.LABTASK.RUN_REACTION = "run_reaction";
